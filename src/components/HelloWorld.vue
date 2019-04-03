1ace07a9c2379dca6286bda28f4e53ae412f8986
Yangkai.Shen <237497819@qq.com>
2019-04-03 16:46:43 +0800

    :sparkles: 添加资源加载引导页

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -98,16 +98,16 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus">
 h3
-  margin 40px 0 0
+    margin 40px 0 0
 
 ul
-  list-style-type none
-  padding 0
+    list-style-type none
+    padding 0
 
 li
-  display inline-block
-  margin 0 10px
+    display inline-block
+    margin 0 10px
 
 a
-  color #42b983
+    color #42b983
 </style>
